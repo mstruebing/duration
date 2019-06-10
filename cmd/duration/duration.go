e754499a5ac4faa440cce2c0c3e4f4bd2ea88b6d
Max Strübing <mxstrbng@gmail.com>
2019-06-10 11:48:16 +0200

    feat(output): print stderr/stdout live
    closes #3

--- cmd/duration/duration.go
+++ cmd/duration/duration.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	tm "github.com/buger/goterm"
 )
 
 // Pads an integer with zeroes to the left
@@ -29,21 +32,43 @@ func getHours(time time.Duration) string {
 	return padTimePart(hours)
 }
 
-// Prints the duration how long a command is already running
-func printDuration() {
-	fmt.Printf("command running since: %s:%s:%s", getHours(0), getMinutes(0), getSeconds(0))
-	start := time.Now()
-
-	ticker := time.NewTicker(time.Second)
-
-	for range ticker.C {
-		currentTime := time.Since(start)
-		fmt.Printf("\rcommand running since: %s:%s:%s", getHours(currentTime), getMinutes(currentTime), getSeconds(currentTime))
-	}
+func clearTime() {
+	tm.MoveCursor(tm.Width()-30, tm.Height())
+	tm.Printf("                               ")
 }
 
-func printCmdOutput(output string) {
-	fmt.Printf("\n\nOutput:\n%s", output)
+func printDuration(start time.Time) {
+	currentTime := time.Since(start)
+
+	tm.MoveCursor(tm.Width()-30, tm.Height())
+	tm.Printf("command running since: %s:%s:%s", getHours(currentTime), getMinutes(currentTime), getSeconds(currentTime))
+	tm.Flush()
+}
+
+func printDurationAndOutput(output *bytes.Buffer) {
+	ticker := time.NewTicker(time.Nanosecond)
+
+	outputAccumulator := ""
+	outputPrint := ""
+
+	start := time.Now()
+	printDuration(start)
+
+	for range ticker.C {
+		currentOutput := output.String()
+
+		if strings.Compare(currentOutput, outputAccumulator) == 1 {
+			outputPrint = strings.Replace(currentOutput, outputAccumulator, "", 1)
+			outputAccumulator = currentOutput
+
+			clearTime()
+			tm.MoveCursor(0, tm.Height())
+			tm.Printf("%s", string(outputPrint))
+			tm.Flush()
+		}
+
+		printDuration(start)
+	}
 }
 
 func main() {
@@ -55,17 +80,19 @@ func main() {
 	program := strings.Join(os.Args[1:2], "")
 	args := strings.Join(os.Args[2:], " ")
 
-	go printDuration()
 	cmd := exec.Command(program, args)
 
-	// Currently the output is printed at the end of the program
-	// We can not differntiate between stdout and stderr anymore
-	// I couldn't find a good solution to print realtime while also
-	// printing the current duration readable until now.
-	output, err := cmd.CombinedOutput()
+	var output bytes.Buffer
+
+	cmd.Stdout = &output
+	cmd.Stderr = &output
+
+	go printDurationAndOutput(&output)
+
+	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
 
-	printCmdOutput(string(output))
+	time.Sleep(time.Second)
 }
